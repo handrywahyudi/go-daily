e33393ab6eae6f05029183d5b4c5c91b3ac92ed9
handry wahyudi <handry19@gmail.com>
2020-04-20 14:46:51 +0700

    Continue interface

--- Beginner/beginner-interfaces.go
+++ Beginner/beginner-interfaces.go
@@ -1,9 +1,46 @@
 package main
 
-import () {
+import (
 	"fmt"
+)
+
+type coordinates interface {
+	xaxis() int
+	yaxis() int
+}
+
+type point2D struct {
+	X int
+	Y int
+}
+
+func (s point2D) xaxis() int {
+	return s.X
+}
+
+func (s point2D) yaxis() int {
+	return s.Y
+}
+
+func findCoordinates(a coordinates) {
+	fmt.Println("X: ", a.xaxis(), "Y: ", a.yaxis())
+}
+
+type coordinate int
+
+func (s coordinate) xaxis() int {
+	return int(s)
+}
+
+func (s coordinate) yaxis() int {
+	return 0
 }
 
 func main() {
+	x := point2D{X: -4, Y: 24}
+	fmt.Println(x)
+	findCoordinates(x)
 
+	y := coordinate(10)
+	findCoordinates(y)
 }
