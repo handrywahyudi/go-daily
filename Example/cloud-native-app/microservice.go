77e58062785e3a3bfcbd2f11825ffa409757273b
handry wahyudi <handry19@gmail.com>
2020-04-30 14:58:14 +0700

    add port function.

--- Example/cloud-native-app/microservice.go
+++ Example/cloud-native-app/microservice.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+        "os"
 )
 
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/cloud", cloud)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string{
+    port := os.Getenv("PORT")
+    if len(port) == 0 {
+        port = "9090"
+    }
+    return ":" + port
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
