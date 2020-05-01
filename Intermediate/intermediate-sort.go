e58525c02611b1eefba49432d4d7e185215f4d6f
handry wahyudi <handry19@gmail.com>
2020-05-01 20:48:12 +0700

    Learn sort.

---
+++ Intermediate/intermediate-sort.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type myStructure struct {
+	person string
+	height int
+	weight int
+}
+
+func main() {
+	mySlice := make([]myStructure, 0)
+	a := myStructure{"muhammad", 170, 95}
+	mySlice = append(mySlice, a)
+	a = myStructure{"handry", 160, 85}
+	mySlice = append(mySlice, a)
+	a = myStructure{"wahyudi", 175, 75}
+	mySlice = append(mySlice, a)
+
+	fmt.Println("0:", mySlice)
+
+	sort.Slice(mySlice, func(i, j int) bool {
+		return mySlice[i].weight < mySlice[j].weight
+	})
+
+	fmt.Println(">:", mySlice)
+}
