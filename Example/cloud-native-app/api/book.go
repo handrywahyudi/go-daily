2da43377ce49d49d65b37fd4232b3b9c7a283a34
handry wahyudi <handry19@gmail.com>
2020-05-02 21:03:01 +0700

    Add route to all books.

--- Example/cloud-native-app/api/book.go
+++ Example/cloud-native-app/api/book.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"net/http"
 )
 
 type myBook struct {
@@ -25,4 +26,19 @@ func FromJSON(data []byte) myBook {
 		panic(err)
 	}
 	return book
+}
+
+var Books = []myBook{
+	myBook{Title: "Go For Dummies", Author: "Linkedin Learning", ISBN: "2321231"},
+	myBook{Title: "Algorithm With Go", Author: "Coursera", ISBN: "2321231"},
+}
+
+func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
+	book, err := json.Marshal(Books)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Add("Content-Type", "application/json; charsetutf-8")
+	w.Write(book)
 }
