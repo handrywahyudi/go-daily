32ba8b232e5a22fcefc5847a912187012fd16d31
handry wahyudi <handry19@gmail.com>
2020-05-07 21:02:21 +0700

    Create function traverse.

--- Intermediate/intermediate-linkedlist.go
+++ Intermediate/intermediate-linkedlist.go
@@ -1,44 +1,51 @@
-package main 
+package main
 
 import (
 	"fmt"
 )
 
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+func addNode(t *Node, v int) int {
+	if root == nil {
+		t = &Node{v, nil}
+		root = t
+		return 0
+	}
+
+	if v == t.Value {
+		fmt.Println("Node already exist:", v)
+		return -1
+	}
+
+	if t.Next == nil {
+		t.Next = &Node{v, nil}
+		return -2
+	}
+	return addNode(t.Next, v)
+}
+
+func traverse(t *Node) {
+	if t == nil {
+		fmt.Println("-> Empty List.")
+		return
+	}
+
+	for t != nil {
+		fmt.Print("%d-> ", t.Value)
+		t = t.Next
+	}
+
+	fmt.Println()
+}
+
+var root = new(Node)
+
 func main() {
-	type Node struct {
-		Value int
-		Next *Node
-	}
-
-	func addNode(t *Node, v int) {
-		if root == nil {
-			t = &Node{v, nil}
-			root = t 
-			return 0
-		}
-
-		if v == t.Value {
-		    fmt.Println("Node already exist:", v)
-		    return -1
-		}
-
-		if t.Next == nil {
-			t.Next = &Node{v, nil}
-			return -2
-		}
-
-		func traverse(t, *Node) {
-			if t == nil {
-				fmt.Println("-> Empty List.")
-				return
-			}
-
-			for t != nil {
-				fmt.Print("%d-> ", t.Value)
-				t = t.Next
-			}
-
-			fmt.Println()
-		}
-	}
+	fmt.Println(root)
+	root = nil
+	fmt.Println(root)
 }
