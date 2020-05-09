dde9f8aed2bdfa42796c3d0363db5d8a0dde7a52
handry wahyudi <handry19@gmail.com>
2020-05-09 22:30:02 +0700

    part two of hash.

--- Intermediate/intermediate-hash.go
+++ Intermediate/intermediate-hash.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	Value int
 	Next  *Node
@@ -9,4 +11,28 @@ type HashTable struct {
 	Table map[int]*Node
 
 	Size int
+}
+
+func hashFunction(i, size int) int {
+	return (i % size)
+}
+
+func insert(hash *HashTable, value int) int {
+	index := hashFunction(value, hash.Size)
+	element := Node{Value: value, Next: hash.Table[index]}
+	hash.Table[index] = &element
+	return index
+}
+
+func traverse(hash *HashTable) {
+	for k := range hash.Table {
+		if hash.Table[k] != nil {
+			t := hash.Table[k]
+			for t != nil {
+				fmt.Printf("%d -> ", t.Value)
+				t = t.Next
+			}
+			fmt.Println()
+		}
+	}
 }
