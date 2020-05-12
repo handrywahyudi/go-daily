97d8e73bd1acd0368c2c8eb625ec92960ae24031
handry wahyudi <handry19@gmail.com>
2020-05-12 21:55:53 +0700

    Continue to learn GC in go.

--- Intermediate/intermediate-gc.go
+++ Intermediate/intermediate-gc.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func printStats(mem runtime.MemStats) {
@@ -11,9 +12,27 @@ func printStats(mem runtime.MemStats) {
 	fmt.Println("mem.TotalAlloc: ", mem.TotalAlloc)
 	fmt.Println("mem.HeapAlloc: ", mem.HeapAlloc)
 	fmt.Println("mem.NumGC: ", mem.NumGC)
-
 }
 
 func main() {
+	var mem runtime.MemStats
 	printStats(mem)
+
+	for i := 0; i < 10; i++ {
+		s := make([]byte, 100000000)
+		if s == nil {
+			fmt.Println("Operation failed!")
+		}
+	}
+	printStats(mem)
+
+	for i := 0; i < 10; i++ {
+		s := make([]byte, 100000000)
+		if s == nil {
+			fmt.Println("Operation failed!")
+		}
+		time.Sleep(5 * time.Second)
+	}
+	printStats(mem)
+
 }
