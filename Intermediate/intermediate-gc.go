5a1dd7aa32bec19d0f484b1fa6b2558994bb5b74
handry wahyudi <handry19@gmail.com>
2020-05-12 21:45:18 +0700

    Learn GC.

---
+++ Intermediate/intermediate-gc.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func printStats(mem runtime.MemStats) {
+	runtime.ReadMemStats(&mem)
+	fmt.Println("mem.Alloc: ", mem.Alloc)
+	fmt.Println("mem.TotalAlloc: ", mem.TotalAlloc)
+	fmt.Println("mem.HeapAlloc: ", mem.HeapAlloc)
+	fmt.Println("mem.NumGC: ", mem.NumGC)
+
+}
+
+func main() {
+	printStats(mem)
+}
