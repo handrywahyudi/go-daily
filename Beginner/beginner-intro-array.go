df879622220d843059271e0b1536710b9380791c
handry wahyudi <handry19@gmail.com>
2020-05-15 22:56:26 +0700

    How to loop array.

--- Beginner/beginner-intro-array.go
+++ Beginner/beginner-intro-array.go
@@ -5,12 +5,13 @@ import (
 )
 
 func main() {
-	var x [5]float64
-	x[0] = 23
-	x[1] = 40
-	x[2] = 33
-	x[3] = 80
-	x[4] = 90
+	x := [5]float64{23, 40, 44, 923, 11}
+	// var x [5]float64
+	// x[0] = 23
+	// x[1] = 40
+	// x[2] = 33
+	// x[3] = 80
+	// x[4] = 90
 	fmt.Println(x)
 
 	var total float64
@@ -18,4 +19,10 @@ func main() {
 		total += x[i]
 	}
 	fmt.Println(total / float64(len(x)))
+
+	var total2 float64 = 0
+	for _, value := range x {
+		total2 += value
+	}
+	fmt.Println(total2 / float64(len(x)))
 }
