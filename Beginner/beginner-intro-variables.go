394ad9d4cbf51b86ef216674281f4c195e443173
handry wahyudi <handry19@gmail.com>
2020-05-15 22:36:14 +0700

    Create example program to convert celcius to fahrenheit.

--- Beginner/beginner-intro-variables.go
+++ Beginner/beginner-intro-variables.go
@@ -9,6 +9,12 @@ var dogsName string = "dog"
 func dog() {
 	fmt.Println("i have a", dogsName)
 }
+
+func cToFah(c float64) float64 {
+	total := (c * 9.0 / 5.0) + 32.0
+	return total
+}
+
 func main() {
 	var x string = "Hello World"
 	fmt.Println(x)
@@ -21,7 +27,14 @@ func main() {
 	fmt.Println("We have a", dogsName)
 	dog()
 
-	const name string = "handry"
-	name = "wahyudi"
-	fmt.Println(name)
+	// const name string = "handry"
+	// name = "wahyudi"
+	// fmt.Println(name)
+
+	/*
+	   Create program to convert C to fahrenheit.
+	*/
+	fmt.Println("This program to convert Celcius to Fahrenheit.")
+	c := cToFah(12)
+	fmt.Println(c)
 }
