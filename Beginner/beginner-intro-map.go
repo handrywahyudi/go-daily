345224cfc4d7ed173d5aabb0067361ca620a689c
handry wahyudi <handry19@gmail.com>
2020-05-16 22:46:36 +0700

    The way to store information using map.

--- Beginner/beginner-intro-map.go
+++ Beginner/beginner-intro-map.go
@@ -5,14 +5,46 @@ import (
 )
 
 func main() {
-	number := make(map[string]int)
-	number["one"] = 1
-	number["two"] = 2
-	number["three"] = 3
-	number["four"] = 4
-	number["five"] = 5
-	number["six"] = 6
+	// number := make(map[string]int)
+	// number["one"] = 1
+	// number["two"] = 2
+	// number["three"] = 3
+	// number["four"] = 4
+	// number["five"] = 5
+	// number["six"] = 6
 
-	delete(number, "one")
-	fmt.Println(number)
+	number := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+	}
+
+	// delete(number, "one")
+	if result, ok := number["seven"]; ok {
+		fmt.Println(result, ok)
+	} else {
+		fmt.Println("The element is not found.")
+	}
+
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+	}
+
+	if el, ok := elements["Be"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
+
 }
