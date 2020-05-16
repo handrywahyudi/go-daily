ff3dd5600ae7524b7a2c626360821cc94c2fdcaf
handry wahyudi <handry19@gmail.com>
2020-05-16 22:09:55 +0700

    How to use append and copy function on slice..

--- Beginner/beginner-intro-slice.go
+++ Beginner/beginner-intro-slice.go
@@ -5,7 +5,14 @@ import (
 )
 
 func main() {
-	x := [5]float64{1, 2, 3, 4, 5}
-	result := x[0:3]
-	fmt.Println(result)
+	// Append function
+	x := []float64{1, 2, 3, 4, 5}
+	y := append(x, 6, 7, 8)
+	fmt.Println(y)
+
+	// Copy function
+	m := []int{1, 2, 3}
+	n := make([]int, 2)
+	copy(n, m)
+	fmt.Println(m, n)
 }
