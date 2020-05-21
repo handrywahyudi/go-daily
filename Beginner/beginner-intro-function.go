4f9f469b77ed58d9570a75fe55697a4d384763b7
handry wahyudi <handry19@gmail.com>
2020-05-21 22:16:42 +0700

    Split code into function.

--- Beginner/beginner-intro-function.go
+++ Beginner/beginner-intro-function.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 )
 
-func main() {
-	number := []float64{43, 23, 45, 63, 21}
-
+func average(number []float64) float64 {
+	// panic("Not implemented")
 	total := 0.0
 	for _, v := range number {
 		total += v
 	}
-	fmt.Println(total / float64(len(number)))
+	return total / float64(len(number))
+}
+
+func main() {
+	number := []float64{5, 5, 5, 5, 5}
+	fmt.Println(average(number))
 }
