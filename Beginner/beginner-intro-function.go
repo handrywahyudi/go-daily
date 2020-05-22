dbdb51291f83cacf4c4f7493b572b3d6b6688043
handry wahyudi <handry19@gmail.com>
2020-05-22 16:58:15 +0700

    How to use variadic function.

--- Beginner/beginner-intro-function.go
+++ Beginner/beginner-intro-function.go
@@ -13,7 +13,18 @@ func average(number []float64) float64 {
 	return total / float64(len(number))
 }
 
+// Use Variadic Function
+func add(args ...int) int {
+	total := 0
+	for _, v := range args {
+		total += v
+	}
+	return total
+}
+
 func main() {
 	number := []float64{5, 5, 5, 5, 5}
 	fmt.Println(average(number))
+
+	fmt.Println(add(add(1, 2, 3)))
 }
