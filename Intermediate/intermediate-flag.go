41cf17e018beed10b0b6a0a053ffc7040aa17f43
handry wahyudi <handry19@gmail.com>
2020-05-30 20:49:21 +0700

    Learn flag in go.

---
+++ Intermediate/intermediate-flag.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	minusO := flag.Bool("o", false, "o")
+	minusC := flag.Bool("c", false, "c")
+	minusK := flag.Int("k", 0, "an integer")
+
+	flag.Parse()
+
+	fmt.Println("o", *minusO)
+	fmt.Println("c", *minusC)
+	fmt.Println("k", *minusK)
+}
