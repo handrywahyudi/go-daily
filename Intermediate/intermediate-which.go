5d77609d64fb7dd0398acd56d4e1b34982b26f7b
handry wahyudi <handry19@gmail.com>
2020-05-31 21:29:15 +0700

    Continue with crete script like tool whic.

--- Intermediate/intermediate-which.go
+++ Intermediate/intermediate-which.go
@@ -4,27 +4,47 @@ import (
     "fmt"
     "flag"
     "os"
-    "string"
+    "strings"
 )
 
 func main() {
-    minusA := flang.bool("a", false, "a")
-    minusB := flang.bool("b", false, "b")
+    minusA := flag.Bool("a", false, "a")
+    minusS := flag.Bool("s", false, "s")
 
-    flag.parse()
+    flag.Parse()
     flags := flag.Args()
     if len(flags) == 0 {
-        fmt.Printlns("Please provide an argument!.")
+        fmt.Println("Please provide an argument!.")
         os.Exit(1)
     }
 
     file := flags[0]
-    foundIt := false
+    fountIt := false
 
     path := os.Getenv("PATH")
     pathslice := strings.Split(path, ":")
     for _, directory := range pathslice {
        fullpath := directory + "/" + file
-
+       fileInfo, err := os.Stat(fullpath)
+       if err == nil {
+           mode := fileInfo.Mode()
+           if mode.IsRegular() {
+               if mode&0111 != 0 {
+                   fountIt = true
+                   if *minusS == true {
+                       os.Exit(0)
+                   }
+                   if *minusA == true {
+                       fmt.Println(fullpath)
+                   }else{
+                       fmt.Println(fullpath)
+                       os.Exit(0)
+                   }
+               }
+           }
+       }
     }
+    if fountIt == false {
+         os.Exit(1)
+   }
 }
